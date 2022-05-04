878e6c620a316900d12e434e706861a5cf230219
snow <snowsuika@gmail.com>
2022-05-04 20:33:47 +0800

    修正 mongo 連線資訊

--- connections/index.js
+++ connections/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
-DB = process.env.DATABASE;
+const DB = process.env.DATABASE.replace('<password>',process.env.DATABASE_PASSWORD)
 
 mongoose
     .connect(DB)
